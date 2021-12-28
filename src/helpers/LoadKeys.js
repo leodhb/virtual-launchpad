d62abf462e860137b9a8e6fb8cfbf1a01e99deaa
Henrique Barbosa <henrikleod@gmail.com>
2021-12-28 00:50:10 -0300

    Range slider state independent

--- src/helpers/LoadKeys.js
+++ src/helpers/LoadKeys.js
@@ -23,40 +23,5 @@ export function LoadKeys(launchpad) {
             },
             false
         );
-
-        
-        function updateSlider(element) {
-          if (element) {
-            let parent = element.parentElement,
-              lastValue = parent.getAttribute('data-slider-value');
-
-            if (lastValue === element.value) {
-              return; // No value change, no need to update then
-            }
-
-            parent.setAttribute('data-slider-value', element.value);
-            let $thumb = parent.querySelector('.range-slider__thumb'),
-              $bar = parent.querySelector('.range-slider__bar'),
-              pct = element.value * ((parent.clientHeight - $thumb.clientHeight) / parent.clientHeight) *100;
-
-            $thumb.style.bottom = `${pct}%`;
-            $bar.style.height = `calc(${pct}% + ${$thumb.clientHeight/2}px)`;
-            //$thumb.textContent = `${element.value * 100}%`;
-          }
-        }
-
-        function initAndSetupTheSliders() {
-          const inputs = [].slice.call(document.querySelectorAll('.range-slider input'));
-          inputs.forEach(input => input.setAttribute('value', '.5'));
-          inputs.forEach(input => updateSlider(input));
-          // Cross-browser support where value changes instantly as you drag the handle, therefore two event types.
-          inputs.forEach(input => input.addEventListener('input', element => updateSlider(input)));
-          inputs.forEach(input => input.addEventListener('change', element => updateSlider(input)));
-        }
-
-        initAndSetupTheSliders()
-
-
-
     };
 }
--- src/ui/components/mixer/range-slider/RangeSlider.jsx
+++ src/ui/components/mixer/range-slider/RangeSlider.jsx
@@ -1,20 +1,49 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./RangeSlider.css";
 
 function RangeSlider(props) {
+  const parentRef = useRef();
+  const thumbRef = useRef();
+  const barRef = useRef();
+
+  const [thumbStyle, setThumbStyle] = useState({});
+  const [barStyle, setBarStyle] = useState({});
+
+  function updateSlider() {
+    const parentHeight = parentRef.current.clientHeight;
+    const thumbHeight  = thumbRef.current.clientHeight;
+
+    let pct = (props.volume * ((parentHeight - thumbHeight) / parentHeight) * 100).toFixed(2);
+
+    setThumbStyle({bottom: `${pct}%`});
+    setBarStyle({height:  `calc(${pct}% + ${thumbHeight / 2}px)`});
+  }
+
+  useEffect(() => {
+    updateSlider();
+  }, [props.volume]);
+
   return (
-    <div className="range-slider">
+    <div className="range-slider" ref={parentRef}>
       <input
         value={props.volume}
         type="range"
         orient="vertical"
-        step=".001"
+        step=".005"
         min="0"
         max="1"
         onChange={props.onChange}
       />
-      <div className="range-slider__bar theme2"></div>
-      <div className="range-slider__thumb"></div>
+      <div
+        className="range-slider__bar theme2"
+        ref={barRef}
+        style={barStyle}
+      ></div>
+      <div
+        className="range-slider__thumb"
+        ref={thumbRef}
+        style={thumbStyle}
+      ></div>
     </div>
   );
 }
