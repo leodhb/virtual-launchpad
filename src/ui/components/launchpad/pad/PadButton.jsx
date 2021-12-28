139b16594cd49237338b55ba2fb501cb059484ec
Henrique Barbosa <henrikleod@gmail.com>
2021-12-28 11:56:18 -0300

    A react friendly howler

--- src/ui/components/launchpad/pad/PadButton.jsx
+++ src/ui/components/launchpad/pad/PadButton.jsx
@@ -1,96 +1,122 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useEventListener } from "../../../../hooks/useEventListener";
-import { useActiveElement } from "../../../../hooks/useActiveElement";
 import { LightenDarkenColor } from "../../../../helpers/Colors";
 import { Howl } from "howler";
 
-import './PadButton.css'
+import "./PadButton.css";
 
 function PadButton(props) {
   const [source, setSource] = useState(props.source);
   const [color, setColor] = useState(props.color);
-  const [name, setName] = useState(props.name)
+  const [name, setName] = useState(props.name);
   const [playing, setPlaying] = useState(false);
   const [loaded, setLoaded] = useState(props.loaded);
   const [loop, setLoop] = useState(props.loop);
   const [mute, setMute] = useState(props.mute);
   const [volume, setVolume] = useState(props.volume);
-  const [clicked, setClicked] = useState(false)
-  
+  const [clicked, setClicked] = useState(false);
+
   const buttonRef = useRef();
 
-  const currentActiveElement = useActiveElement();
+  const [sound, setSound] = useState(null);
+
+  useEffect(() => {
+    destroyHowler();
+
+    if (typeof Howl !== "undefined") {
+      setSound(
+        new Howl({
+          src: [source],
+          format: ["mp3"],
+          onplay: () => setPlaying(true),
+          onend: () => setPlaying(false),
+          onload: () => setLoaded(true),
+          volume: volume,
+          loop: loop,
+          mute: mute,
+        })
+      );
+    }
+    return () => {
+      destroyHowler();
+    };
+  }, []);
 
   //Change volume when the mixer channel fader is moved
   useEffect(() => {
-    if(volume != props.volume) {
-      setVolume(props.volume)
-      ReactHowler.current.volume(props.volume)
+    if (volume != props.volume) {
+      setVolume(props.volume);
+      sound.volume(props.volume);
     }
   }, [props.volume]);
 
-  useEffect(() => playing ? handleReset() : ReactHowler.current.stop(), [playing])
+  useEffect(() => {
+    if (sound) {
+      playing ? handleReset() : sound.stop();
+    }
+  }, [sound, playing]);
 
-  const handleToggle = () => loop ? handleLoop() : handleSample()
+  const handleToggle = () => (loop ? handleLoop() : handleSample());
 
   const handleSample = () => {
     setClicked(true);
-    setTimeout(() => { setClicked(false) }, 50)
+    setTimeout(() => {
+      setClicked(false);
+    }, 50);
 
-    if(playing) {
-      handleReset();
-    }
+    if (playing) handleReset();
 
     setPlaying(true);
-  }
+  };
 
   const handleLoop = () => {
     let playingsong = true;
 
-    if(playing) playingsong = false;
+    if (playing) playingsong = false;
 
-    setPlaying(playingsong)
-  }
+    setPlaying(playingsong);
+  };
 
   const handleReset = () => {
-    ReactHowler.current.stop()
-    ReactHowler.current.play()
-  }
+    sound.stop();
+    sound.play();
+  };
 
-  const handleOnPlay = () => setPlaying(true)
-
-  const handleOnLoad = () => setLoaded(true)
-
-  const handleOnEnd = () => setPlaying(false)
-
-  const ReactHowler = useRef(new Howl({
-    src: [source],
-    format: ['mp3'],
-    onplay: handleOnPlay,
-    onend: handleOnEnd,
-    onload: handleOnLoad,
-    volume: volume,
-    loop: loop,
-    mute: mute
-  }));
+  const destroyHowler = () => {
+    if (sound) {
+      sound.off();
+      sound.unload();
+      sound.stop();
+      setSound(null);
+    }
+  };
 
   const lightenColor = LightenDarkenColor(color, 50);
-  const darkerColor  = LightenDarkenColor(color, -30);
+  const darkerColor = LightenDarkenColor(color, -30);
 
-  const activeStyles = (playing && !clicked) ? {
-    boxShadow: `0 0 50px 8px ${lightenColor}, inset 0 0 4px 4px rgba(0, 0, 0, 0.2)`,
-    backgroundColor: color,
-    borderBottom: `solid 2px ${darkerColor}`,
-    borderRight: `solid 2px ${darkerColor}`
-  } : {};
+  const activeStyles =
+    playing && !clicked
+      ? {
+          boxShadow: `0 0 50px 8px ${lightenColor}, inset 0 0 4px 4px rgba(0, 0, 0, 0.2)`,
+          backgroundColor: color,
+          borderBottom: `solid 2px ${darkerColor}`,
+          borderRight: `solid 2px ${darkerColor}`,
+        }
+      : {};
 
   return (
-      <>
-        <button className="pad" ref={buttonRef} id={props.uuid} style={activeStyles} onClick={handleToggle}>{props.keycode.substr(-1)}</button>
-        <p className="sample-description">{name}</p>
-      </>
+    <>
+      <button
+        className="pad"
+        ref={buttonRef}
+        id={props.uuid}
+        style={activeStyles}
+        onClick={handleToggle}
+      >
+        {props.keycode.substr(-1)}
+      </button>
+      <p className="sample-description">{name}</p>
+    </>
   );
-
 }
 
 export default PadButton;
